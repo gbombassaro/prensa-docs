b96516029d5dd66edd38d7b166f31e26c67728c3
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-10-10 22:20:49 -0300

    Adiciona arquivo de build

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+// rollup.config.js
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import sass from 'rollup-plugin-sass';
+
+export default [
+  {
+    input: 'components/index.js',
+    external: ['react', 'prop-types', 'sass', 'classnames'],
+    output: [
+      {
+        file: 'index.js',
+        format: 'cjs',
+        globals: {
+          react: 'React'
+        },
+        name: 'prensa-docs'
+      }
+    ],
+    plugins: [ 
+      babel({
+        exclude: 'node_modules/**, stories/**',
+      }),
+      json(),
+      resolve(),
+      sass(),
+    ]
+  },
+];
